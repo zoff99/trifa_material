8bbc78f5252da0db77cb249027010975aee2ddbd
zoff99 <zoff99@users.noreply.github.com>
2024-02-03 13:27:59 +0100

    remove more stuff

--- src/main/java/com/exception/NotificationException.java
+++
@@ -1,9 +0,0 @@
-package com.exception;
-
-public class NotificationException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public NotificationException(String exception) {
-		super(exception);
-	}
-}
--- src/main/java/com/zoffcc/applications/trifa/EmojiStrAndName.kt
+++
@@ -1,7 +0,0 @@
-package com.zoffcc.applications.trifa
-
-class EmojiStrAndName
-{
-    var char: String = ""
-    var name: String = ""
-}
